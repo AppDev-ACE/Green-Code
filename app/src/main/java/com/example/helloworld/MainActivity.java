82607e3a8f863376074448d907078315548ad18a
Tejeswar-16 <tejeswarsakthidaran@gmail.com>
2026-01-20 00:05:56 +0530

    implemented basic hello world with button click

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.helloworld;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    public TextView txtHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txtHelloWorld = (TextView) findViewById(R.id.txtHelloWorld);
+    }
+
+    public void handleBtnClick(View v){
+        txtHelloWorld.setText("Hello World! GreenCode!!!");
     }
 }
